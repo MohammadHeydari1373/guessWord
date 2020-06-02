648c19b8108298795be218b808117c208245ddb2
محمدرضا حیدری <52635406+MohammadHeydari1373@users.noreply.github.com>
2020-06-03 02:08:05 +0430

    Add files via upload

---
+++ HadsKalamat/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace HadsKalamat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBxBala = new System.Windows.Forms.GroupBox();
+            this.lblTedad = new System.Windows.Forms.Label();
+            this.picBoxAnswer = new System.Windows.Forms.PictureBox();
+            this.picBoxPart = new System.Windows.Forms.PictureBox();
+            this.btnHadsKalame = new System.Windows.Forms.Button();
+            this.txtKalame = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWrong = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.بازیجدیدToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblNew = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBxBala.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxAnswer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPart)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBxBala
+            // 
+            this.groupBxBala.BackColor = System.Drawing.Color.Transparent;
+            this.groupBxBala.Controls.Add(this.lblTedad);
+            this.groupBxBala.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.groupBxBala.Location = new System.Drawing.Point(27, 81);
+            this.groupBxBala.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBxBala.Name = "groupBxBala";
+            this.groupBxBala.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBxBala.Size = new System.Drawing.Size(540, 152);
+            this.groupBxBala.TabIndex = 0;
+            this.groupBxBala.TabStop = false;
+            this.groupBxBala.Enter += new System.EventHandler(this.groupBxBala_Enter);
+            // 
+            // lblTedad
+            // 
+            this.lblTedad.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblTedad.Location = new System.Drawing.Point(402, 22);
+            this.lblTedad.Name = "lblTedad";
+            this.lblTedad.Size = new System.Drawing.Size(131, 45);
+            this.lblTedad.TabIndex = 0;
+            // 
+            // picBoxAnswer
+            // 
+            this.picBoxAnswer.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxAnswer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxAnswer.Location = new System.Drawing.Point(574, 54);
+            this.picBoxAnswer.Name = "picBoxAnswer";
+            this.picBoxAnswer.Size = new System.Drawing.Size(148, 124);
+            this.picBoxAnswer.TabIndex = 1;
+            this.picBoxAnswer.TabStop = false;
+            // 
+            // picBoxPart
+            // 
+            this.picBoxPart.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxPart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBoxPart.Location = new System.Drawing.Point(558, 226);
+            this.picBoxPart.Name = "picBoxPart";
+            this.picBoxPart.Size = new System.Drawing.Size(104, 166);
+            this.picBoxPart.TabIndex = 2;
+            this.picBoxPart.TabStop = false;
+            // 
+            // btnHadsKalame
+            // 
+            this.btnHadsKalame.BackColor = System.Drawing.Color.Transparent;
+            this.btnHadsKalame.FlatAppearance.BorderSize = 0;
+            this.btnHadsKalame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHadsKalame.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnHadsKalame.Location = new System.Drawing.Point(148, 270);
+            this.btnHadsKalame.Name = "btnHadsKalame";
+            this.btnHadsKalame.Size = new System.Drawing.Size(132, 51);
+            this.btnHadsKalame.TabIndex = 3;
+            this.btnHadsKalame.Text = "حدس کلمه";
+            this.btnHadsKalame.UseVisualStyleBackColor = false;
+            this.btnHadsKalame.Click += new System.EventHandler(this.btnHadsKalame_Click);
+            // 
+            // txtKalame
+            // 
+            this.txtKalame.Font = new System.Drawing.Font("B Mitra", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtKalame.Location = new System.Drawing.Point(303, 283);
+            this.txtKalame.MaxLength = 1;
+            this.txtKalame.Name = "txtKalame";
+            this.txtKalame.Size = new System.Drawing.Size(100, 30);
+            this.txtKalame.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(80, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 44);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "حروف اشتباه حدس زده شده";
+            // 
+            // lblWrong
+            // 
+            this.lblWrong.BackColor = System.Drawing.Color.Transparent;
+            this.lblWrong.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblWrong.Location = new System.Drawing.Point(296, 348);
+            this.lblWrong.Name = "lblWrong";
+            this.lblWrong.Size = new System.Drawing.Size(256, 29);
+            this.lblWrong.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.بازیجدیدToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(913, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // بازیجدیدToolStripMenuItem
+            // 
+            this.بازیجدیدToolStripMenuItem.Name = "بازیجدیدToolStripMenuItem";
+            this.بازیجدیدToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.بازیجدیدToolStripMenuItem.Text = "بازی جدید";
+            this.بازیجدیدToolStripMenuItem.Click += new System.EventHandler(this.بازیجدیدToolStripMenuItem_Click);
+            // 
+            // lblNew
+            // 
+            this.lblNew.BackColor = System.Drawing.Color.Transparent;
+            this.lblNew.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblNew.Location = new System.Drawing.Point(184, 392);
+            this.lblNew.Name = "lblNew";
+            this.lblNew.Size = new System.Drawing.Size(478, 44);
+            this.lblNew.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(721, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "mr.heidari1373@gmail.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::HadsKalamat.Properties.Resources.Backg1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(913, 481);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNew);
+            this.Controls.Add(this.lblWrong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKalame);
+            this.Controls.Add(this.btnHadsKalame);
+            this.Controls.Add(this.picBoxPart);
+            this.Controls.Add(this.picBoxAnswer);
+            this.Controls.Add(this.groupBxBala);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("B Nazanin", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "بازی کلمات";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBxBala.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxAnswer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPart)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBxBala;
+        private System.Windows.Forms.Label lblTedad;
+        private System.Windows.Forms.PictureBox picBoxAnswer;
+        private System.Windows.Forms.PictureBox picBoxPart;
+        private System.Windows.Forms.Button btnHadsKalame;
+        private System.Windows.Forms.TextBox txtKalame;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWrong;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem بازیجدیدToolStripMenuItem;
+        private System.Windows.Forms.Label lblNew;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HadsKalamat/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace HadsKalamat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #region Var
+        SoundPlayer mediaSoundPlayer = new SoundPlayer();
+        private string List = "";
+        List<Label> labels = new List<Label>(); 
+        private string word = "";
+        private int count = 0; 
+        
+        #endregion
+        #region Enum Rregion
+        enum Parts
+        {
+            Dar, Head , Shirt,Legs,HeadFeet,Shoes,Eyes
+        }  
+
+
+        #endregion
+        #region  DrawBodyParts
+        private void DrawBodyParts(Parts bp)
+        {   if (bp == Parts.Dar)
+            picBoxPart.Image = Properties.Resources._1;
+        else if (bp == Parts.Head)
+            picBoxPart.Image = Properties.Resources._2;
+        else if (bp == Parts.Shirt)
+            picBoxPart.Image = Properties.Resources._3;
+        else if (bp == Parts.Legs)
+            picBoxPart.Image = Properties.Resources._4;
+        else if (bp == Parts.HeadFeet)
+            picBoxPart.Image = Properties.Resources._5;
+        else if (bp == Parts.Shoes)
+            picBoxPart.Image = Properties.Resources._6;
+        else if (bp == Parts.Eyes)
+            picBoxPart.Image = Properties.Resources._7;
+        }
+
+        #endregion
+        #region GetRandomWord
+
+        private string GetRandomWord()
+        {   
+            List  = File.ReadAllText("word.txt",Encoding.UTF8) ; 
+            
+            string[] word = List.Split('\n') ;
+            Random rand = new Random();
+            string Word1 = word[rand.Next(0, word.Length - 1)];
+            
+
+            if (Word1 == word[0])
+            {
+                lblNew.Text = " آن چیست که روز را کنار پنجره می گذراند، و هنگام غذا کنار میز است و شب ها ناپدید می شود؟";
+
+            }
+            if (Word1 == word[1])
+            {
+                lblNew.Text = "این چیه که مورچه داره ، اما مار نداره ؟";
+
+            }
+            if (Word1 == word[2])
+            {
+                lblNew.Text = "برگ سبز چمنی، ورق ورق تو می شکنی؟";
+
+            }
+            if (Word1 == word[3])
+            {
+                lblNew.Text = "آن چیست که جان دارد و نفس نمی کشد؟";
+
+            }
+            if (Word1 == word[4])
+            {
+                lblNew.Text = "آن چیست که پر دارد و پرواز نمی کند؟";
+
+            }
+            if (Word1 == word[5])
+            {
+                lblNew.Text = " کدام رازداری است که تا سرش را نبریم رازش را نمی گوید؟";
+
+            }
+            if (Word1 == word[6])
+            {
+                lblNew.Text = " سازنده این بازی کیست ؟";
+
+            }
+            if (Word1 == word[7])
+            {
+                lblNew.Text = " ﺍﻭﻥ ﭼﻴﻪ ﺩﻭ ﺣﺮﻓﻴﻪ، ﻛﺮﺩﻧﻴﻪ ﺣﺮﻑ ﺩومش هم «ﺱ» ؟";
+
+            }
+            if (Word1 == word[8])
+            {
+                lblNew.Text = "اﻭﻥ ﭼﻴﻪ ﺳﻪ ﺣﺮﻓﻴﻪ ﺣﺮﻑ ﺳﻮﻣﺶ «ﺭ» ﻫﻤﻪ ﻫﻢ ﺩﺭﺍﺯشو ﺩﻭﺳﺖ ﺩﺍﺭﻥ؟";
+
+            }
+            if (Word1 == word[9])
+            {
+                lblNew.Text = " اگر سر پرگار گیج برود چه می‌کشد؟";
+
+            }
+            if (Word1 == word[10])
+            {
+                lblNew.Text = "اگر شما به من غذا بدهید، من زندگی می کنم. اگر به من نوشیدنی بدهی، من می میرم. من چی هستم؟";
+
+            }
+           
+            return Word1;
+            
+
+         
+           // return Word1; 
+        }
+        #endregion
+        #region make label 
+         void makeLabel()
+        {
+            word = GetRandomWord();
+
+            char[] chars = word.ToCharArray();
+            int between = 300 / chars.Length - 1;
+            for (int i = 0; i < chars.Length-1; i++)
+            {
+                labels.Add(new Label());
+                labels[i].Location = new Point(i * between + 20, 60);
+                labels[i].Text = "_";
+                labels[i].Parent = groupBxBala; 
+                labels[i].BringToFront();
+                
+                labels[i].CreateControl();
+                
+                labels[i].Font = new Font("tahoma", 14); 
+
+
+            }
+            lblTedad.Text = "تعداد حروف : "+(chars.Length-1).ToString(); 
+             
+        }
+        #endregion
+
+         private void Form1_Shown(object sender, EventArgs e)
+         {
+             makeLabel();
+         }
+         #region button Logic
+         private void btnHadsKalame_Click(object sender, EventArgs e)
+         {
+             
+             try
+             {
+                 char letter = txtKalame.Text.ToLower().ToCharArray()[0];
+                 if (!char.IsLetter(letter))
+                 {
+                     string Location = @"error.wav";
+                     mediaSoundPlayer.SoundLocation = Location;
+                     mediaSoundPlayer.Play();
+                     MessageBox.Show("فقط باید حرف را وارد کنید : ", "اخطار",MessageBoxButtons.OK , MessageBoxIcon.Error );
+                     txtKalame.Text = null; 
+                 }
+                 else if(word.Contains(letter))
+                 {
+                     string Location = @"Backs.wav";
+                     mediaSoundPlayer.SoundLocation = Location;
+                     mediaSoundPlayer.Play();
+                     char[] letters = word.ToCharArray();
+                     for (int i = 0; i < letters.Length-1; i++)
+                     {
+                         if (letters[i] == letter)
+                         
+                             labels[i].Text = letter.ToString();
+                             picBoxAnswer.Image = Properties.Resources.okay;
+                             txtKalame.Text = null;
+                            
+                     }
+                         foreach (Label l in labels)
+                         
+                             if (l.Text == "_")
+                             
+                                 return;
+                             
+                         
+                         Location = @"win.wav";
+                         mediaSoundPlayer.SoundLocation = Location;
+                         mediaSoundPlayer.Play();
+                         MessageBox.Show("شما برنده شدید" ,"تبریک " ,MessageBoxButtons.OK , MessageBoxIcon.Information );
+                         reset() ; 
+
+                     
+                 }
+                    else
+                 {
+                     //کلمات اشتباه و ساخته شدن طناب دار
+                     string Location = @"steak.wav";
+                     mediaSoundPlayer.SoundLocation = Location;
+                     mediaSoundPlayer.Play();
+                     picBoxAnswer.Image = Properties.Resources.wrong;
+                     lblWrong.Text +=letter.ToString() + " , ";
+                     txtKalame.Text = null;
+                     DrawBodyParts((Parts)count);
+                       
+                     count++;
+                     //حالت باخت
+                     if (count==7)
+                     {
+                         Location = @"lose.wav";
+                     mediaSoundPlayer.SoundLocation = Location;
+                     mediaSoundPlayer.Play();
+                     MessageBox.Show("متاسفانه شما باختید ", "مرگ ", MessageBoxButtons.OK, MessageBoxIcon.Stop);    
+                         reset() ;
+                         count = 0; 
+                     }
+                     else 
+                     {
+                         if(txtKalame.Text == null)
+                         {
+                         return;
+                         }
+                     }
+
+                 }
+
+                 }
+                
+             
+             catch 
+             {
+
+                 MessageBox.Show("حتما باید یک کلمه باید واردکنید ", "اخطار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+             }
+ 
+         }
+#endregion
+     
+         private void groupBxBala_Enter(object sender, EventArgs e)
+         {
+
+         }
+
+         #region reset
+         private void reset()
+         {
+             picBoxPart.Image = null;
+             picBoxAnswer.Image = null;
+             lblWrong.Text = null;
+             GetRandomWord();
+            // lblNew.Text = null;
+             lblTedad.Text = null;
+             count = 0;
+
+
+             makeLabel();
+         }
+         #endregion
+
+         private void بازیجدیدToolStripMenuItem_Click(object sender, EventArgs e)
+         {
+             reset();
+             count = 0;
+         }
+    }
+}
